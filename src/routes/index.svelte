932853c45f9dbc951740d15f59b683bc17f1529f
Paulmicha <paul.michalet@gmail.com>
2020-10-17 16:44:10 -0300

    Debug netlify identity : add window.netlifyIdentity.on(init)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,18 @@
 <script>
+	import { onMount } from 'svelte';
 	import successkid from 'images/successkid.jpg';
+
+	onMount(() => {
+		if (window.netlifyIdentity) {
+			window.netlifyIdentity.on("init", user => {
+				if (!user) {
+					window.netlifyIdentity.on("login", () => {
+						document.location.href = "/admin/";
+					});
+				}
+			});
+		}
+	});
 </script>
 
 <style>

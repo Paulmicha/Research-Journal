596df3eb459b3ade39279b8ba422a5cda6933e9f
Paulmicha <paul.michalet@gmail.com>
2020-12-14 16:55:49 -0300

    feat(index): add sort links

--- scripts/experiments/search_index/lib/parsing.js
+++ scripts/experiments/search_index/lib/parsing.js
@@ -315,7 +315,11 @@ const build_channels_urls_index = () => {
 			doc.reactions = [];
 			if ('reactions' in message) {
 				doc.reactions = parseReactions(message.reactions);
-				console.log(doc);
+			}
+
+			// Debug.
+			if ('author' in doc && doc.author.includes('Lordon')) {
+				console.log(doc.names);
 			}
 
 			index.documents.push(doc);
--- src/components/content/ExpChannelsIndex.svelte
+++ src/components/content/ExpChannelsIndex.svelte
@@ -25,8 +25,20 @@
 	/**
 	 * Populates the multi-select field items.
 	 */
-	$: getSelectItems = () => {
-		documents.forEach(doc => {
+	const getSelectItems = (docs) => {
+		docs.forEach(doc => {
+
+			// Special case : Emojis.
+			if ('reactions' in doc && doc.reactions.length) {
+				doc.reactions.forEach(reaction => {
+					selectItems.push({
+						key: 'reactions',
+						value: reaction.name,
+						label: reaction.name
+					});
+				});
+			}
+
 			multiSelectSearchesInKeys.forEach(key => {
 				if (key in doc) {
 					doc[key].split(',').forEach(val => {
@@ -39,8 +51,8 @@
 						key = key.replace('channel', 'tags');
 
 						selectItems.push({
+							key,
 							value: val,
-							key: key,
 							label: `${val} <span style="color:grey">(${key})</span>`
 						});
 					});
@@ -49,7 +61,6 @@
 		});
 
 		// Remove duplicates.
-		// const dedup = [...new Set(selectItems)];
 		let seen = {};
 		const dedup = selectItems.filter(item =>
 			seen.hasOwnProperty(item.label) ? false : (seen[item.label] = true)
@@ -57,8 +68,6 @@
 
 		// Sort alphabetically (using translitteration).
 		dedup.sort((a, b) => a.value.localeCompare(b.value));
-
-		// return dedup.filter(String);
 
 		return dedup;
 	};
@@ -107,6 +116,11 @@
 				let allFilterValuesMatch = true;
 
 				selectedFilterItems.forEach(selectedFilterItem => {
+					// Emojis.
+					// if (!('reactions' in result) || !result.reactions.includes(selectedFilterItem.value)) {
+					// 	allFilterValuesMatch = false;
+					// }
+
 					if (!(selectedFilterItem.key in result) || !result[selectedFilterItem.key].includes(selectedFilterItem.value)) {
 						allFilterValuesMatch = false;
 					}
@@ -133,6 +147,11 @@
 				let anyFilterValueMatches = false;
 
 				selectedFilterItems.forEach(selectedFilterItem => {
+					// Emojis.
+					// if ('reactions' in result && result.reactions.includes(selectedFilterItem.value)) {
+					// 	anyFilterValueMatches = true;
+					// }
+
 					if (selectedFilterItem.key in result && result[selectedFilterItem.key].includes(selectedFilterItem.value)) {
 						anyFilterValueMatches = true;
 					}
@@ -155,7 +174,91 @@
 		// console.log(`clearSelectFilter() : (${filterOp}, ${documents.length} results)`);
 
 		documentsStore.set(documents);
-  };
+	};
+
+	/**
+	 * Helper to count total number of reactions.
+	 */
+	const getReactionsCount = doc => {
+		if (!('reactions' in doc) || !doc.reactions.length) {
+			return 0;
+		}
+		let count = 0;
+		doc.reactions.forEach(reaction => {
+			count += reaction.count;
+		});
+		return count;
+	}
+
+	/**
+	 * Helper to return value by given key or fallback value if doc has no key.
+	 *
+	 * fallback value defaults to empty string.
+	 */
+	const getDocValOr = (doc, key, fallback) => {
+		if (!fallback) {
+			fallback = '';
+		}
+		if (!(key in doc)) {
+			return fallback;
+		}
+		return doc[key];
+	}
+
+	/**
+	 * Sorts results on given key.
+	 *
+	 * Inverts order if already active.
+	 */
+	const sortBy = (e, key) => {
+		const isOff = e.target.classList.contains('is-off');
+		const isAsc = e.target.classList.contains('is-asc');
+		const isDesc = e.target.classList.contains('is-desc');
+
+		let newState;
+
+		switch (key) {
+			case 'date_shared':
+				if (isOff || isAsc) {
+					documents.sort((a, b) => new Date(b.date_shared).getTime() - new Date(a.date_shared).getTime());
+					newState = 'is-desc';
+				} else {
+					documents.sort((a, b) => new Date(a.date_shared).getTime() - new Date(b.date_shared).getTime());
+					newState = 'is-asc';
+				}
+				break;
+			case 'reactions':
+				if (isOff || isAsc) {
+					documents.sort((a, b) => getReactionsCount(b) - getReactionsCount(a));
+					newState = 'is-desc';
+				} else {
+					documents.sort((a, b) => getReactionsCount(a) - getReactionsCount(b));
+					newState = 'is-asc';
+				}
+				break;
+			default:
+				if (isOff || isDesc) {
+					documents.sort((a, b) => getDocValOr(a, key, 'z').localeCompare(getDocValOr(b, key, 'z')));
+					newState = 'is-asc';
+				} else {
+					documents.sort((a, b) => getDocValOr(b, key, 'z').localeCompare(getDocValOr(a, key, 'z')));
+					newState = 'is-desc';
+				}
+				break;
+		}
+
+		// Update docs in their new order.
+		documentsStore.set(documents);
+
+		// Sync sort links state.
+		const allSortLinks = Array.from(e.target.closest('thead').querySelectorAll('.sort'));
+		allSortLinks.forEach(sortLink => {
+			sortLink.classList.remove('is-asc', 'is-desc');
+			sortLink.classList.add('is-off');
+		});
+		e.target.classList.remove('is-off');
+		e.target.classList.add(newState);
+	};
 </script>
 
 <!-- <ExternalScript url="https://unpkg.com/lunr/lunr.js" on:loaded="{onLoaded}" /> -->
@@ -167,7 +270,7 @@
 
 <form>
 	<div class="select">
-		<Select items={getSelectItems()} isMulti={true}
+		<Select items={getSelectItems($documentsStore)} isMulti={true}
 			on:select={applySelectFilter}
 			on:clear={clearSelectFilter}
 			bind:selectedValue={selectedFilterItems}
@@ -202,13 +305,49 @@
 <div class="full-vw">
 	<table>
 		<thead>
-			<th>Date shared</th>
-			<th>Reactions</th>
-			<th>Title</th>
-			<th>Type</th>
+			<th>
+				<a class="sort is-desc" on:click={e => sortBy(e, 'date_shared')} title="Trier par date de partage">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Date shared
+				</a>
+			</th>
+			<th>
+				<a class="sort is-off" on:click={e => sortBy(e, 'reactions')} title="Trier par nombre de réactions">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Reactions
+				</a>
+			</th>
+			<th>
+				<a class="sort is-off" on:click={e => sortBy(e, 'title')} title="Trier par titre">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Title
+				</a>
+			</th>
+			<th>
+				<a class="sort is-off" on:click={e => sortBy(e, 'type')} title="Trier par titre">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Type
+				</a>
+			</th>
 			<th>Tags</th>
-			<th>Author</th>
-			<th>Names</th>
+			<th>
+				<a class="sort is-off" on:click={e => sortBy(e, 'author')} title="Trier par auteur">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Author
+				</a>
+			</th>
+			<th>
+				<a class="sort is-off" on:click={e => sortBy(e, 'names')} title="Trier par noms">
+					<span class="is-asc">↑</span>
+					<span class="is-desc">↓</span>
+					Names
+				</a>
+			</th>
 			<th>Description</th>
 		</thead>
 		<tbody>
@@ -276,5 +415,22 @@
 	}
 	.desc {
 		max-width: 76ch;
+	}
+	.sort {
+		display: flex;
+		cursor: pointer;
+	}
+	.sort > span{
+		padding-right: var(--space-s);
+	}
+	.sort.is-off .is-asc,
+	.sort.is-off .is-desc {
+		display: none;
+	}
+	:global(.sort.is-asc .is-desc) {
+		display: none;
+	}
+	.sort.is-desc .is-asc {
+		display: none;
 	}
 </style>
